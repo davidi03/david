05fc5f79cf23609365845c3820f17963f24dc81d
David Barabas <you@example.com>
2021-08-11 21:29:09 +0200

    commit

---
+++ src/components/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app>
+    <div class="header">
+      <router-view></router-view>
+      <h1>heading1</h1>
+    </div>
+  </v-app>
+</template>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-filter</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-navigation-drawer v-model="drawer" app fixed absolute left temporary>
+        <v-list nav dense>
+          <v-list-item-group active-class="orange--text text--accent-4">
+            <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              dense
+              router
+              :to="item.route"
+            >
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-title>{{ item.title }}</v-list-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { icon: "mdi-speedometer", title: "Dashboard", route: "/Dashboard" },
+        { icon: "mdi-tools", title: "About", route: "/views/About.vue" },
+      ],
+    };
+  },
+
+  watch: {
+    group() {
+      this.drawer = true;
+    },
+  },
+};
+</script>
